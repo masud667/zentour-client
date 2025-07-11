197db5f788cc6ff179bae1f73f4a30cb701c895b
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-07-11 19:42:14 +0600

    basic vite tailwind daisyui react instalation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-5xl text-center pt-50'>
+          <h1>Zen Tour</h1>
+      <progress className="progress w-56 text-amber-400"></progress>
+    </div>
+  )
+}
+
+export default App
